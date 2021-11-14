1a3094ac9fbbc1c4fb5a13ee0f2c8048b4b677c0
Gaurav Kamat <gaurav.kamat.999@gmail.com>
2021-11-14 22:58:26 +0530

    Delete User implemented

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,6 @@ router.get('/', db.getUsers);
 router.get('/:id', db.getUserById);
 router.post('/', db.createUser)
 router.put('/:id', db.updateUser)
+router.delete('/:id', db.deleteUser)
 
 module.exports = router;
